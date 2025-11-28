a982fc7c5a9be2de7aea098d081424b1bfcc0478
dhruv <dhruvslashstar@gmail.com>
2025-11-28 16:11:31 +0530

    add SpeedInsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { ContentfulProvider } from "../context/ContentfulContext";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Dhruv Sheladiya - Portfolio",
@@ -10,9 +11,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <ContentfulProvider>
-          {children}
-        </ContentfulProvider>
+        <ContentfulProvider>{children}</ContentfulProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
